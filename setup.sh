89d9deecc5298d74a315cd45b23791f2d725991a
aabidsofi19 <mailtoaabid01@gmail.com>
2023-03-05 14:22:32 +0530

    Added Prompt installation

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ export PATH=$HOME/.cargo/bin:$PATH
 #Install Other Packages
 sudo apt-get install neofetch ripgrep  wget tmux exa
 
-# Install NvChad
-git clone https://github.com/NvChad/NvChad ./dots/config/nvim --depth 1 
-
 # Install Rust ToolChain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# Install StarshipPrompt
+curl -sS https://starship.rs/install.sh | sh
+
+# Install NvChad
+git clone https://github.com/NvChad/NvChad ./dots/config/nvim --depth 1 
 # Install Some Packages 
 python install_packages.py
 
