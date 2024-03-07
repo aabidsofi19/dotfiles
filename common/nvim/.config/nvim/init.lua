7f198e0a9c9a48f1e58914c6723a5e49bfd0d924
aabidsofi19 <mailtoaabid01@gmail.com>
2024-03-07 16:05:14 +0530

    add neovim config
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

---
+++ common/nvim/.config/nvim/init.lua
@@ -0,0 +1,27 @@
+require("colorscheme")
+require("options")
+require("plugins")
+require("lsp")
+require("keymaps").setup()
+require("workspace")
+
+vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap('i', '<C-X>', 'copilot#Accept("<CR>")', {expr=true, silent=true,script=true})
+
+
+-- Set default file format to Unix (LF line endings) for all files
+
+vim.cmd[[au! BufRead,BufNewFile * setlocal fileformat=unix]]
+if vim.g.neovide then
+	-- Set transparency and background color (title bar color)
+	-- vim.o.guifont = "Iosevka:h14"
+	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_floating_blur = 1
+	-- vim.g.transparency = 0.6
+	vim.g.neovide_floating_blur_amount_x = 10
+	vim.g.neovide_floating_blur_amount_y = 10
+	-- vim.o.winblend = 60
+	-- vim.o.pumblend = 60
+	vim.g.neovide_refresh_rate = 60
+end
+
