829ec816a9b8c9e35feaa61b3d605816646d0414
Ryan Yin <xiaoyin_c@qq.com>
2024-06-17 15:52:01 +0530

    add nix flakes and homemanager
    Signed-off-by: Ryan Yin <xiaoyin_c@qq.com>

---
+++ nix/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager, used for managing user configuration
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
+
+  outputs = { self, nixpkgs,home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.aabid = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+
+      ];
+    };
+  };
+}
